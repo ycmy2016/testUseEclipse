505042741a3fcf5b5ae906e4c7783f71fd8df8af
ycmy2016 <lovebit2010@qq.com>
2017-01-21 10:56:23 +0800

    第二次提交

---
+++ src/com/ycmy/testgit/TestGit2.java
@@ -0,0 +1,10 @@
+package com.ycmy.testgit;
+
+public class TestGit2 {
+	
+	public static void main(String[] args) {
+		System.out.println("hello git21 ");
+		System.out.println("hello git22 ");
+	}
+
+}
